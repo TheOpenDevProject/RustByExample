7ebfe95e9fa741997c68e96aa91ebd6d9eff338a
TheOpenDevProject <tkgs28@gmail.com>
2016-04-16 21:29:41 +1000

    Added rust tutorial 09 - variable_sized_types :D

---
+++ variable_sized_types/isize_tutorial/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let another_size:u32 = std::u32::MAX;
+    let yani:u64 = std::u64::MAX;
+
+    let my_unknown_size:usize = std::usize::MAX;
+
+    println!("{:?}",another_size );
+    println!("{:?}",yani );
+    println!("{:?}",my_unknown_size);
+
+}
