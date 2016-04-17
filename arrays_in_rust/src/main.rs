c47f02aec1451db1a9d0dcb456b0446eb5165070
TheOpenDevProject <tkgs28@gmail.com>
2016-04-17 22:08:42 +1000

    Added source code for two new tutorials

---
+++ arrays_in_rust/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut shopping_list = [1,3,4,7,8,12];
+    println!("{:?}",shopping_list);
+    shopping_list[2] = 500;
+    println!("{:?}",shopping_list);
+
+}
---
+++ floating_point_types/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x:f32 = std::f32::MAX;
+    let y:f64 = std::f64::MAX;
+    println!("{:?}",x );
+    println!("{:?}",y );
+}
