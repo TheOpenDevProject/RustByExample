6b4589a11bb9c59ea532e47be6babd7b9467ad07
TheOpenDevProject <tkgs28@gmail.com>
2016-05-11 00:28:08 +1000

    Added two new tutorials

---
+++ array_len/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let z = [1,2,3];
+    println!("{:?}",z.len());
+}
---
+++ array_subscript/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let m_array = [55,66,27];
+    println!("{:?}",m_array.len());
+    println!("{:?}",m_array[0]);
+    print!("{:?}",m_array[3]);
+}
